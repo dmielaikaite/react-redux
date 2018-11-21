2fdeb4a1e8264f5d8f7190ac002b77f87f8c3762
Donata Mielaikaite <d.mielaikaite@gmail.com>
2018-11-21 22:26:37 +0100

    modified mews item css

--- src/components/mainPage/new_list_item.js
+++ src/components/mainPage/new_list_item.js
@@ -4,16 +4,15 @@ const NewsItem = (props) => {
 
   return(
     <li className="list-group-item">
-      <div className="articleHeader">
+      <div className="article-header">
         <small className="articlePublishDate">{props.article.publishedAt}</small>
         <h5 className="mb-1">{props.article.title}</h5>
       </div>
-      <div className="article-image">
+      <div className="article-content">
+        <div className="article-description col-md-8">{props.article.description}</div>
         <img className="img-thumbnail" src={props.article.urlToImage}/>
       </div>
-      <div className="mb-1 article-description">
-        {props.article.description}
-      </div>
+
     </li>
   )
 
