cdd9ab72360cc96013fd3615a2980f48e5b6415c
Donata Mielaikaite <d.mielaikaite@gmail.com>
2018-11-25 20:52:31 +0100

    working with semantic ui comments

--- src/components/learn/native_component.js
+++ src/components/learn/native_component.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import faker from 'faker';
 
+import NativeCommentDetails from './native_comment_detail';
+
 const NativeComponenet = () => {
   return (
     <div>
       <div className="badge badge-secondary" id="nativeComponent">
         my first native component
-      </div>
+      </div >
       <div className="ui container comments">
-        <div className="comment">
-          <a href="/" className="avatar">
-            <img alt="avatar" src={faker.image.avatar()} />
-          </a>
-          <div className="content">
-            <a href="/" className="author">
-              Sam
-            </a>
-            <div className="metadata">
-              <span className="date"> Today 6:00pm</span>
-            </div>
-            <div className="text"> Nice post! </div>
-          </div>
-        </div>
+        <NativeCommentDetails
+          picture={faker.image.avatar()}
+          author="name1"
+          publishedDate="11-09-2018"
+          content="content1" />
+        <NativeCommentDetails
+          picture={faker.image.avatar()}
+          author="name2"
+          publishedDate="11-07-2018"
+          content="content2" />
+        <NativeCommentDetails
+          picture={faker.image.avatar()}
+          author="name3"
+          publishedDate="11-04-2018"
+          content="content3" />
       </div>
     </div>
   );
