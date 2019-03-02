c7ba169712a16162965915af21dbd5012580d519
Donata Mielaikaite <d.mielaikaite@gmail.com>
2019-03-02 19:11:38 +0100

    created first reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: 'no scrubs', duration: '4:00' },
+    { title: 'macarena', duration: '2:00' },
+    { title: 'all star', duration: '3:10' },
+    { title: 'moon', duration: '6:13' }
+  ]
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload
+  }
+
+  return selectedSong
+};
