14a65f08060661ea964ccce68d6123926f5daef6
Donata Mielaikaite <d.mielaikaite@gmail.com>
2019-03-02 19:50:56 +0100

    added some css

--- src/components/learn/songs/index.js
+++ src/components/learn/songs/index.js
@@ -4,10 +4,35 @@ import { connect } from 'react-redux';
 // import { selectSong } from '../../../actions';
 
 class Songs extends Component {
+
+  renderList() {
+    return this.props.songs.map((song) => {
+      return(
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">
+              Select
+            </button>
+          </div>
+
+          <div className="content">
+            {song.title}
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     return(
-      <div>
-        songs
+      <div className="ui container grid">
+        <div className="ui row">
+          <div className="column eight wide">
+            <div className="ui divided list">
+              {this.renderList()}
+            </div>
+          </div>
+        </div>
       </div>
     )
   };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,10 @@ import { combineReducers } from 'redux';
 
 const songsReducer = () => {
   return [
-    { title: 'no scrubs', duration: '4:00' },
-    { title: 'macarena', duration: '2:00' },
-    { title: 'all star', duration: '3:10' },
-    { title: 'moon', duration: '6:13' }
+    { title: "no scrubs", duration: "4:00" },
+    { title: "macarena", duration: "2:00" },
+    { title: "all star", duration: "3:10" },
+    { title: "moon", duration: "6:13" }
   ]
 };
 
